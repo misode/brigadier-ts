60d523d45c318aa06ad1cdfd2f2cc6dd27040405
Misode <Misoloo64@gmail.com>
2022-04-25 13:59:05 +0200

    Fix #5 zero results in commands

--- test/CommandDispatcher.test.ts
+++ test/CommandDispatcher.test.ts
@@ -1,7 +1,25 @@
-import { CommandDispatcher } from '../src'
+import { CommandDispatcher, literal } from '../src'
 
 describe('CommandDispatcher', () => {
 	test('create', () => {
 		const dispatcher = new CommandDispatcher()
+	})
+
+	test('execute', () => {
+		const dispatcher = new CommandDispatcher()
+		dispatcher.register(literal('foo')
+			.executes(() => 2)
+		)
+		const result = dispatcher.execute('foo', undefined)
+		expect(result).toEqual(2)
+	})
+
+	test('execute (zero result)', () => {
+		const dispatcher = new CommandDispatcher()
+		dispatcher.register(literal('foo')
+			.executes(() => 0)
+		)
+		const result = dispatcher.execute('foo', undefined)
+		expect(result).toEqual(0)
 	})
 })
